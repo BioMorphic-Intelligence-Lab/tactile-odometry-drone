8f3ff74d1ad192dcf2eb395135f9afc0a3a29122
Micha-TUD <micha.schuster@tu-dresden.de>
2023-09-25 19:02:56 +0200

    bugfix odometry filter

--- onboard/src/odometry_filter/src/odometry_filter.cpp
+++ onboard/src/odometry_filter/src/odometry_filter.cpp
@@ -10,7 +10,7 @@ OdometryFilter::OdometryFilter()
   // this->_timer = this->create_wall_timer(1.0 / this->_frequency * 1s,
   //                                      std::bind(&OdometryFilter::_timer_callback, this));
   this->_trackball_subscription = this->create_subscription<geometry_msgs::msg::PointStamped>(
-      "/trackball/position",
+      "/trackballX19/position",
       rclcpp::SensorDataQoS(),
       std::bind(&OdometryFilter::_trackball_callback, this, std::placeholders::_1));
   this->_imu_subscription = this->create_subscription<sensor_msgs::msg::Imu>(
@@ -36,7 +36,9 @@ OdometryFilter::OdometryFilter()
 
         */
   this->R_EO = personal::common::rot_z(-M_PI / 2) * personal::common::rot_y(-M_PI / 2);
+  this->R_WE_0 = personal::common::rot_y(M_PI);
   this->pos_odom_last = Eigen::Vector3d(0, 0, 0);
+  this->pos_wall_last = Eigen::Vector3d(0, 0, 0);
   printf("starting...");
 }
 
@@ -63,8 +65,6 @@ void OdometryFilter::_imu_callback(const sensor_msgs::msg::Imu::SharedPtr msg)
   this->quat_imu.x() = msg->orientation.x;
   this->quat_imu.y() = msg->orientation.y;
   this->quat_imu.z() = msg->orientation.z;
-
-  this->odom_to_wall(this->_trackball_pos, this->quat_imu);
 }
 Eigen::Vector3d OdometryFilter::odom_to_wall(Eigen::Vector3d pos_odom, Eigen::Quaterniond quat_imu)
 {
@@ -76,12 +76,25 @@ Eigen::Vector3d OdometryFilter::odom_to_wall(Eigen::Vector3d pos_odom, Eigen::Qu
   Eigen::Vector3d pos_e = this->R_EO * (pos_odom - this->pos_odom_last);
   Eigen::Vector3d pos_wall = this->pos_wall_last + R_WE * this->R_WE_0 * pos_e;
 
+  /*
+    Eigen::IOFormat CleanFmt(4, 0, ", ", "\n", "[", "]");
+    std::cout << "pos_e" << std::endl
+              << pos_e << std::endl;
+    std::cout << "R_WE" << std::endl
+              << R_WE.format(CleanFmt) << std::endl;
+    std::cout << "R_WE_0" << std::endl
+              << this->R_WE_0.format(CleanFmt) << std::endl;
+    std::cout << "pos_wall" << std::endl
+              << pos_wall << std::endl;
+              */
+
   std_msgs::msg::Float64 roll_msg;
 
   roll_msg.data = roll;
   this->_roll_publisher_->publish(roll_msg);
 
-  this->pos_odom_last = pos_wall;
+  this->pos_odom_last = pos_odom;
+  this->pos_wall_last = pos_wall;
 
   return pos_wall;
 }
@@ -92,7 +105,7 @@ int main(int argc, char *argv[])
   rclcpp::executors::MultiThreadedExecutor executor;
   auto node = std::make_shared<OdometryFilter>();
   executor.add_node(node);
-  RCLCPP_INFO(node->get_logger(), "Starting client node, shut down with CTRL-C");
+  RCLCPP_INFO(node->get_logger(), "Starting odometry_filter node, shut down with CTRL-C");
   executor.spin();
   return 0;
 }
