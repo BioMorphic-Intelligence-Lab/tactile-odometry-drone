0a17cf6c85d9171882d614395af9e3eba62a75bf
Anton Bredenbeck <a.bredenbeck@tudelft.nl>
2023-09-27 10:28:07 +0200

    By default only dockerize the trackball/imu application

--- dockerize.sh
+++ dockerize.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 docker buildx build --platform linux/arm64,linux/amd64 -t antbre/tactile-odometry-drone -f trackball_and_imu.devel.dockerfile --push .
-docker buildx build --platform linux/arm64,linux/amd64 -t antbre/test-trajectory -f controller.devel.dockerfile --push .
+#docker buildx build --platform linux/arm64,linux/amd64 -t antbre/test-trajectory -f controller.devel.dockerfile --push .
